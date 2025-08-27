83497d81cecef03f269161a354f4b7f0e817c2cb
Luis Antonio Cuaquentzi Avendaño <luis.cuaquentzi@gmail.com>
2025-08-27 09:57:37 -0600

    feat: adding three in a row

---
+++ src/src/types/threeInARow.ts
@@ -0,0 +1,11 @@
+export interface ThreeInARow {
+    board: Box[][];
+    isEndOfGame: boolean;
+    winningBoxes: number[][];
+}
+
+export enum Box {
+    EMPTY = 0,
+    USER = 1,
+    SERVER = 2,
+}
