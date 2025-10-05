c2e194ccd7915719741bdc2821efae523e19602c
Luis Antonio Cuaquentzi Avendaño <luis.cuaquentzi@gmail.com>
2025-10-05 00:53:41 -0600

    fix: sudoku.module

--- src/sudoku/sudoku.module.ts
+++ src/sudoku/sudoku.module.ts
@@ -3,12 +3,13 @@ import { SudokuController } from './sudoku.controller';
 import { SudokuService } from './sudoku.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { Sudoku, SudokuSchema } from '../repositories/sudoku.repository';
+import { SudokuGame } from './sudoku-game';
 
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: Sudoku.name, schema: SudokuSchema }]),
   ],
   controllers: [SudokuController],
-  providers: [SudokuService],
+  providers: [SudokuService, SudokuGame],
 })
 export class SudokuModule {}
